7bec7ccc4f47bf70ba86e5d15d19755f4d749e0e
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-23 22:07:55 +0200

    Chore: remove dummy tests

--- lib/maxo_uni_repo.ex
+++ lib/maxo_uni_repo.ex
@@ -1,5 +1,2 @@
 defmodule MaxoUniRepo do
-  def greeting do
-    "Welcome to Maxo!"
-  end
 end
--- lib/maxo_uni_repo_test.exs
+++ lib/maxo_uni_repo_test.exs
@@ -1,8 +1,4 @@
 defmodule MaxoUniRepoTest do
   use ExUnit.Case
   use MnemeDefaults
-
-  test "greeting" do
-    auto_assert("Welcome to Maxo!" <- MaxoUniRepo.greeting())
-  end
 end
